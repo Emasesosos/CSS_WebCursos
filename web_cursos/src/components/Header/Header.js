eecefac97eb911e05d48b0aa2336456f4cf3262d
Emasesosos <j.emmanuel.martinez.e@hotmail.com>
2020-03-18 23:20:02 -0600

    Configuración Inicial de Gulp, Compilando SASS, Agregando Watch y Compilando estilos del Header

---
+++ web_cursos/src/components/Header/Header.js
@@ -0,0 +1,39 @@
+import React from 'react';
+import logo from './../../img/logo.jpg';
+import cart from './../../img/cart.png';
+import curso1 from './../../img/curso1.jpg';
+
+const Header = () => {
+    return (
+            <header className="header contenedor">
+                <div className="logo">
+                    <img src={logo} alt="logo"/>
+                </div>
+                <div className="carrito-compras">
+                    <img src={cart} alt="cart"/>
+                    <div className="contenido oculto">
+                        <table id="lista-carrito">
+                            <thead>
+                                <tr>
+                                    <th>Imagen</th>
+                                    <th>Nombre</th>
+                                    <th>Precio</th>
+                                </tr>
+                            </thead>
+                            <tbody> 
+                                <tr>
+                                    <td>
+                                        <img src={curso1} alt="curso1"/>
+                                    </td>
+                                    <td>CSS Grid y Flexbox Construye + 10 Proyectos</td>
+                                    <td>$15</td>  
+                                </tr>
+                            </tbody>
+                        </table>
+                        <a href="/" className="boton">Vaciar Carrito</a>
+                    </div>
+                </div>
+            </header>
+    );
+}
+export default Header;
